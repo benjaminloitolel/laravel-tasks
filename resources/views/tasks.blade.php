34a96d8be56bd3803861c80ac9b38124e2370edb
benjaminloitolel <152763368+benjaminloitolel@users.noreply.github.com>
2023-12-03 19:05:45 +0300

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    New Task please...
+                    New Task please update again...
                 </div>
 
                 <div class="panel-body">
